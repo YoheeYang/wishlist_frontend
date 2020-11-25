f5b67cc008bb8e1f73b6f0b871f5446558039dce
Xi Yang <starry.yohee@gmail.com>
2020-11-25 16:43:02 +0800

    homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      item-key="name"
+      class="elevation-1"
+      :search="search"
+      :custom-filter="filterOnlyCapsText"
+    >
+      <template v-slot:top>
+        <v-text-field
+          v-model="search"
+          label="Search (UPPER CASE ONLY)"
+          class="mx-4"
+        ></v-text-field>
+      </template>
+     
+    </v-data-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  computed:{
+    headers(){
+      return[
+        {text:'Name',value:'name'},
+        {text:'Brand', value:'brand'},
+        {text:'Type', value:'item_type'},
+        {text:'Desire', value:'desire'},
+        {text:'Sales', value:'sales'},
+        {text:'Expire', value:'expire_date'}
+      ]
+    }
+
   }
+
 }
 </script>
